bcd13cc56aee1cdf8a4e58d30d6145d7312922fa
Erronatus <erronatus@users.noreply.github.com>
2026-03-09 17:48:54 -0500

    fix: change output to server for API route support

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://erronatus.com',
-  output: 'static',
+  output: 'server',
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
